571b4489de20f6a0b3dd755a1483d3124e3e335a
Joni Laitinen <joni.laitinen@gmail.com>
2021-11-11 14:47:59 +0200

    Exercise 5 done plus review and credits

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -12,16 +12,83 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+// const smallBox = (
+//   <div
+//     className="box box--small"
+//     style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}
+//   >
+//     small lightblue box
+//   </div>
+// )
+// const mediumBox = (
+//   <div
+//     className="box box--medium"
+//     style={{backgroundColor: 'pink', fontStyle: 'italic'}}
+//   >
+//     medium pink box
+//   </div>
+// )
+// const largeBox = (
+//   <div
+//     className="box box--large"
+//     style={{backgroundColor: 'orange', fontStyle: 'italic'}}
+//   >
+//     large orange box
+//   </div>
+// )
 
+// Extra credit 1
+function Box({className = '', style, size, ...otherProps}) {
+  // if (size === 'medium') {
+  //   className = 'box--medium'
+  // } else if (size === 'large') {
+  //   className = 'box--large'
+  // } else if (size === 'small') {
+  //   className = 'box--small'
+  // }
+
+  // Above if can be done in oneliner
+  const sizeClassName = size ? `box--${size}` : ''
+  return (
+    <div
+      className={`box ${className} ${sizeClassName}`}
+      style={{fontStyle: 'italic', ...style}}
+    >
+      {otherProps.children}
+    </div>
+  )
+}
+
+// function App() {
+//   return (
+//     <div>
+//       <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
+//         small lightblue box
+//       </Box>
+//       <Box className="box--medium" style={{backgroundColor: 'pink'}}>
+//         medium pink box
+//       </Box>
+//       <Box className="box--large" style={{backgroundColor: 'orange'}}>
+//         large orange box
+//       </Box>
+//     </div>
+//   )
+// }
+
+// Extra credit 2
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box size="small" style={{backgroundColor: 'lightblue'}}>
+        small lightblue box
+      </Box>
+      <Box size="medium" style={{backgroundColor: 'pink'}}>
+        medium pink box
+      </Box>
+      <Box size="large" style={{backgroundColor: 'orange'}}>
+        large orange box
+      </Box>
+      <Box>sizeless box</Box>
     </div>
   )
 }
